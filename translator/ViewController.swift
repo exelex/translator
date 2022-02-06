4a6d4d1de118c1a3acd7c8783348d364263cb43a
Alexey-frontend <lexer.work@gmail.com>
2022-02-07 01:37:59 +0300

    update position of elements

--- translator/ViewController.swift
+++ translator/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     let networkManager = NetworkManager()
     
+    var translateArray: [Translate] = []
+    
+    @IBOutlet weak var btnHistory: UIButton!
+    
+    
     @IBOutlet weak var sourceField: UITextField!
     
     @IBOutlet weak var resultField: UITextField!
@@ -19,7 +24,6 @@ class ViewController: UIViewController {
     @IBAction func btn(_ sender: Any) {
         if let source = sourceField.text {
             self.networkManager.fetchCurrentTranslate(forText: source) { currentTranslate in
-                print(currentTranslate.translate)
                 DispatchQueue.main.async{
                     self.resultField.text = currentTranslate.translate
                 }
@@ -30,7 +34,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        btnHistory.titleEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
         
     }
     
